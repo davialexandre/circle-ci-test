5dafe385288bb23fefc0e235116b880a1c8c55e6
Davi Alexandre <davi@davialexandre.com.br>
2019-07-03 14:35:33 -0300

    fix some style errors

--- test.php
+++ test.php
@@ -1,4 +1,11 @@
-<?php function     ($adsadsa , $dsadsadsadsa) 
-{ $a = 1;
-$b=3;
-$c=false;}
+<?php
+
+/**
+ * @param $adsadsa
+ * @param $dsadsadsadsa
+ */
+function my_function($adsadsa , $dsadsadsadsa) {
+    $a = 1;
+    $b = 3;
+    $c = FALSE;
+}
